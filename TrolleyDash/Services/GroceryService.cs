d382ae30c413619391809a95f3dfc337dbc7997e
Paddy McGlone <paddymcglone@gmail.com>
2020-04-01 19:59:35 +0100

    Adding the missing file.

---
+++ TrolleyDash/Services/GroceryService.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using TrolleyDash.Data;
+using TrolleyDash.Models;
+
+namespace TrolleyDash.Services
+{
+    public class GroceryService : IGroceryService
+    {
+
+        #region Properties
+        private readonly ApplicationDbContext _context { get; set; }
+        #endregion
+
+        #region Constructor
+        public GroceryService(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+        #endregion
+
+        #region Methods
+        public IEnumerable<Grocery> GetAllGroceriesToBeFetched()
+        {
+            return _context.Groceries.Where(g => !g.Fetched).ToList();
+        }
+        #endregion
+    }
+}
