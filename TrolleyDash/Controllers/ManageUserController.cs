8ad2eeea8f35a3002266898425aafbd6aff82ec0
Paddy McGlone <paddymcglone@gmail.com>
2020-04-05 20:10:45 +0100

    Adding the ManageUserController.

---
+++ TrolleyDash/Controllers/ManageUserController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrolleyDash.Controllers
+{
+    public class ManageUserController : Controller
+    {
+        
+    }
+}
---
+++ TrolleyDash/Controllers/ManageUsersController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrolleyDash.Controllers
+{
+    public class ManageUserController : Controller
+    {
+        
+    }
+}
